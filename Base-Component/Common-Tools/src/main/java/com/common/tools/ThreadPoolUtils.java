53af9d01ed41555c98e7abf213c6118d57a7e452
spring-gamal <zhuzf_25@163.com>
2017-07-06 12:35:17 +0800

    提交线程池类

---
+++ Base-Component/Common-Tools/src/main/java/com/common/tools/ThreadPoolUtils.java
@@ -0,0 +1,25 @@
+package com.common.tools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.beans.factory.InitializingBean;
+
+public class ThreadPoolUtils implements InitializingBean
+{
+    public static final int HTREAD_POOL_DEFAULT_SIZE = 30;
+
+    private static ExecutorService executorService;
+
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        executorService = Executors.newFixedThreadPool(HTREAD_POOL_DEFAULT_SIZE);
+
+    }
+
+    public static ExecutorService getExecutorService()
+    {
+        return executorService;
+    }
+}
