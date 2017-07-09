e07bb3ba6686ad1a234976bc0dc0e42b9ba4afdc
spring-gamal <zhuzf_25@163.com>
2017-07-10 00:31:20 +0800

    test

--- Base-Component/Common-Tools/src/main/java/com/common/tools/ThreadPoolUtils.java
+++ Base-Component/Common-Tools/src/main/java/com/common/tools/ThreadPoolUtils.java
@@ -13,10 +13,13 @@ import org.springframework.beans.factory.InitializingBean;
 public class ThreadPoolUtils implements InitializingBean
 {
     /**
-     * 线程池默认打小
+     * 线程池默认大小
      */
     private static final int HTREAD_POOL_DEFAULT_SIZE = 30;
 
+    /**
+     * 线程池服务类
+     */
     private static ExecutorService executorService;
 
     public void afterPropertiesSet() throws Exception
